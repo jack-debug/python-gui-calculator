a345d54dff16671b191e618438287c4f2a6d37c8
jack-debug <55571956+jack-debug@users.noreply.github.com>
2019-11-01 10:20:14 +1100

    Update gui-calculator.py

--- gui-calculator.py
+++ gui-calculator.py
@@ -13,6 +13,11 @@ def button(source, side, text, command=None):
     return storeObj
  
 class app(Frame):
+display = StringVar()
+        Entry(self, relief=RIDGE, textvariable=display,
+          justify='right'
+          , bd=30, bg="powder blue").pack(side=TOP,
+                                          expand=YES, fill=BOTH)
     def __init__(self):
         Frame.__init__(self)
         self.option_add('*Font', 'arial 20 bold')
